d192b54c36642f530af3f2d4ff90963eeb4fedd6
Luca Morgado <luca.morgado.pro@gmail.com>
2025-03-26 16:50:36 +0100

    added loggers

--- Connected/connected.go
+++ Connected/connected.go
@@ -3,6 +3,7 @@ package Connected
 import (
 	"github.com/ElderLab/SSO-Middleware/internal/utils"
 	"github.com/gofiber/fiber/v2"
+	"log"
 )
 
 type Config struct {
@@ -25,7 +26,10 @@ func configDefault(config ...Config) Config {
 func New(config Config) fiber.Handler {
 	// Return new middleware
 	return func(c *fiber.Ctx) error {
+		log.Println("Entering Middleware")
 		// If Filter is not nil and returns true, skip middleware
+		log.Println("Checking Filter")
+		log.Println(config.Filter != nil && config.Filter(c))
 		if config.Filter != nil && config.Filter(c) {
 			return c.Next()
 		}
@@ -33,20 +37,25 @@ func New(config Config) fiber.Handler {
 		token := c.Get("Authorization")
 		// if the token is empty, return unauthorized
 		if token == "" {
+			log.Println("Token is empty Unauthorized")
 			return c.Status(fiber.StatusUnauthorized).JSON(fiber.Map{"error": "Unauthorized"})
 		}
 		token = token[7:]
 		// query the SSO service
 		connected, claims := utils.QuerySSO(token)
+		log.Println("Connected: ", connected)
 		// if the SSO service is not connected, return unauthorized
 		if !connected {
+			log.Println("Not Connected Unauthorized")
 			return c.Status(fiber.StatusUnauthorized).JSON(fiber.Map{"error": "Unauthorized"})
 		}
 		// if GetClaims is true, set the claims to the context
 		if config.GetClaims {
+			log.Println("Getting Claims")
 			c.Locals("claims", claims)
 		}
 		if config.GetUser {
+			log.Println("Getting Full User")
 			// get the full user information
 			err, fullUser := utils.GetFullUser(claims)
 			// if there is an error, return internal server error
@@ -56,6 +65,7 @@ func New(config Config) fiber.Handler {
 			// set the full user information to the context
 			c.Locals("user", fullUser)
 		}
+		log.Println("Exiting Middleware")
 		// Continue stack
 		return c.Next()
 	}
